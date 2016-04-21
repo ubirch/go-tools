41b0489d2e7a4c3d58d4ce75bc6127446ccc6ba2
Falko Zurell <falko.zurell@gmail.com>
2016-04-21 09:07:29 +0200

    improved handling of unset variables

--- concat-labels.sh
+++ concat-labels.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
-DEPENDENCY_LABEL=`env | grep GO_DEPENDENCY_LABEL_ | cut -d= -f2`
-OWN_LABEL=`env | grep GO_PIPELINE_LABEL | cut -d= -f2`
+OWN_LABEL=`env | grep GO_PIPELINE_LABEL | cut -d= -f2 | sed 's/\s//g' | tr -d '\n'`
 
-echo "${DEPENDENCY_LABEL:=0}_${OWN_LABEL:=00}"
+DEPENDENCY_LABEL=`env | grep GO_DEPENDENCY_LABEL_ | cut -d= -f2 | sed 's/\s//g' | tr -d '\n'`
+
+
+if [ -z ${DEPENDENCY_LABEL} ]; then
+  echo ${OWN_LABEL}
+else
+  echo "${DEPENDENCY_LABEL}_${OWN_LABEL}"
+fi
